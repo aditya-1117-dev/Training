a8ddda5ec755d162bb116ae770972235aed447f3
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2024-12-02 12:32:13 +0530

    Update TypeScript version and remove ts-ignore

--- Exercise/TS/Dishes/dishes-api-main/utils/index.ts
+++ Exercise/TS/Dishes/dishes-api-main/utils/index.ts
@@ -4,7 +4,6 @@ export const getRandomIngredients = (): Record<string, number> => {
     const availableIngredients: Record<string, number> = {};
     for (const ingredientName in ingredientList) {
         if (Math.random() > 0.10) {
-            // @ts-ignore
             const ingredient = ingredientList[ingredientName];
             const randomAmount = Math.ceil(Math.random() * (ingredient.max - ingredient.min) + ingredient.min);
             availableIngredients[ingredientName] = parseFloat(randomAmount.toFixed(2));
