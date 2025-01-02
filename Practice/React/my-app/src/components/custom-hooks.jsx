3e1d931594c5aefcc13b2898c5b67c730041be1c
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-02 18:42:10 +0530

    Add practice code for custom hooks

---
+++ Practice/React/my-app/src/components/custom-hooks.jsx
@@ -0,0 +1,36 @@
+import {useEffect, useState} from "react";
+
+export default function useOnlineStatus() {
+    const [isOnline, setIsOnline] = useState(true);
+    useEffect(() => {
+        function handleOnline() {
+            setIsOnline(true);
+        }
+        function handleOffline() {
+            setIsOnline(false);
+        }
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+        return () => {
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+        };
+    }, []);
+    return isOnline;
+}
+
+
+export function useFormInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleChange(e) {
+        setValue(e.target.value);
+    }
+
+    return {
+        value,
+        onChange: handleChange
+    };
+}
+
+
