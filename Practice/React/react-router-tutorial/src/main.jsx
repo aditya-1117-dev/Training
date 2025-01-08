8991f34ca4b462c198f9856f747b34a0a887c3f8
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-08 19:13:22 +0530

    Add filtering & error handling for react router tutorial

--- Practice/React/react-router-tutorial/src/main.jsx
+++ Practice/React/react-router-tutorial/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Contact, {
     loader as contactLoader,
+    action as contactAction
 } from "./routes/contact";
 
 import {
@@ -35,23 +36,29 @@ const router = createBrowserRouter([
         loader: rootLoader,
         action : rootAction,
         children: [
-            { index: true, element: <Index /> },
             {
-                path: "contacts/:contactId",
-                element: <Contact />,
-                loader: contactLoader,
-            },
-            {
-                path: "contacts/:contactId/edit",
-                element: <EditContact />,
-                loader: contactLoader,
-                action: editAction,
-            },
-            {
-                path: "contacts/:contactId/destroy",
-                action: destroyAction,
-                errorElement: <div>Oops! There was an error.</div>,
-            },
+                errorElement: <ErrorPage/>,
+                children: [
+                    {index: true, element: <Index/>},
+                    {
+                        path: "contacts/:contactId",
+                        element: <Contact/>,
+                        loader: contactLoader,
+                        action: contactAction,
+                    },
+                    {
+                        path: "contacts/:contactId/edit",
+                        element: <EditContact/>,
+                        loader: contactLoader,
+                        action: editAction,
+                    },
+                    {
+                        path: "contacts/:contactId/destroy",
+                        action: destroyAction,
+                        errorElement: <div>Oops! There was an error.</div>,
+                    }
+                ]
+            }
         ],
     },
 ]);
@@ -62,3 +69,42 @@ createRoot(document.getElementById("root")).render(
     </StrictMode>
 );
 
+
+//
+// import {
+//     createRoutesFromElements,
+//     createBrowserRouter,
+//     Route,
+// } from "react-router-dom";
+//
+// const router = createBrowserRouter(
+//     createRoutesFromElements(
+//         <Route
+//             path="/"
+//             element={<Root />}
+//             loader={rootLoader}
+//             action={rootAction}
+//             errorElement={<ErrorPage />}
+//         >
+//             <Route errorElement={<ErrorPage />}>
+//                 <Route index element={<Index />} />
+//                 <Route
+//                     path="contacts/:contactId"
+//                     element={<Contact />}
+//                     loader={contactLoader}
+//                     action={contactAction}
+//                 />
+//                 <Route
+//                     path="contacts/:contactId/edit"
+//                     element={<EditContact />}
+//                     loader={contactLoader}
+//                     action={editAction}
+//                 />
+//                 <Route
+//                     path="contacts/:contactId/destroy"
+//                     action={destroyAction}
+//                 />
+//             </Route>
+//         </Route>
+//     )
+// );
--- Practice/React/react-router-tutorial/src/routes/root.jsx
+++ Practice/React/react-router-tutorial/src/routes/root.jsx
@@ -1,9 +1,13 @@
-import {Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation} from "react-router-dom";
+import {Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation, useSubmit} from "react-router-dom";
 import {createContact, getContacts,} from "../contacts";
+import {useEffect} from "react";
 
-export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
+export async function loader({ request }) {
+    // const contacts = await getContacts();
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
 }
 
 export async function action() {
@@ -13,37 +17,58 @@ export async function action() {
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );
+
+    console.log(navigation.location)
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
 
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
+                    <Form id="search-form" role="search">
                         <input
                             id="q"
+                            className={searching ? "loading" : ""}
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
                         />
                         <div
                             id="search-spinner"
                             aria-hidden
-                            hidden={true}
+                            hidden={!searching}
                         />
                         <div
                             className="sr-only"
                             aria-live="polite"
                         ></div>
-                    </form>
+                    </Form>
                     {/*<form method="post">*/}
                     {/*    <button type="submit">New</button>*/}
                     {/*</form>*/}
                     <Form method="post">
-                        <button type="submit">New-Submit Button</button>
+                        <button type="submit">New-Submit </button>
                     </Form>
                 </div>
                 <nav>
