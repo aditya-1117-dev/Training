68b6451844765a264504610ce3e8a351d802990f
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-23 19:06:17 +0530

    Feat add homepage with filter products

---
+++ Exercise/React/react-product-listing-exercise/src/Pages/Home/Home.tsx
@@ -0,0 +1,42 @@
+import {useFormInputBox} from "../../Utility/CustomHooks/formInput.tsx";
+import {lazy, Suspense, useState} from "react";
+import "./Home.css";
+import {Link} from "react-router-dom";
+import useFetch from "../../Utility/CustomHooks/fetchData.tsx";
+
+const ListProducts = lazy(() => import("../../Components/ListProducts.tsx"));
+const ListCategories = lazy(() => import("../../Components/ListCategories.tsx"));
+
+const Home = () =>{
+    const search = useFormInputBox("");
+    const [category, setCategory] = useState("all");
+    const query = search.value.length > 0 ? `search?q=${search.value}` : '';
+
+    const productData = useFetch(`https://dummyjson.com/products/${query}`);
+    const categoryData = useFetch(`https://dummyjson.com/products/category-list`);
+
+    // const productDataByCategories = useFetch(`https://dummyjson.com/products/category/${category}`);
+
+    return (
+        <>
+            <div className="container">
+                <h1>Product Listing</h1>
+                <div className="search-container">
+                    <input type="text" placeholder="Search for products" value={search.value} onChange={search.onChange} className="search-input"/>
+                    <div className="category-cart">
+                        <select value={category} onChange={(e) => setCategory(e.target.value)} className="category-select">
+                            <ListCategories categoryData={categoryData}/>
+                        </select>
+                        <Link to={'cart'}> <h2 className="cart-heading"> <span>User</span>'s Cart</h2> </Link>
+                    </div>
+                </div>
+            </div>
+
+            <Suspense fallback={<div> ... loading</div>}>
+                <ListProducts productData={productData}/>
+            </Suspense>
+        </>
+    )
+}
+
+export default Home;
