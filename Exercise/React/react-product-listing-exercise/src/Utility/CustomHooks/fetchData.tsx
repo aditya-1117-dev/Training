7bcc5a4aed8db50aa28a1290ecf57cfe76832d40
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-23 19:02:24 +0530

    Feat add custom hook for fetch api

---
+++ Exercise/React/react-product-listing-exercise/src/Utility/CustomHooks/fetchData.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url : string) => {
+    const [data, setData] = useState(null);
+    // const [error, setError] = useState(null);
+    // const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                const data = await res.json();
+                setData(data);
+            } catch (err) {
+                // setError(err.message);
+            } finally {
+                // setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+
+    return { data };
+};
+
+export default useFetch;
