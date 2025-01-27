b56a1c5d0052f957d372d1ec130a31133cf6b0a5
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-27 18:15:29 +0530

    Refactor navbar using react router dom

--- Exercise/React/react-product-listing-exercise/src/App.tsx
+++ Exercise/React/react-product-listing-exercise/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
 import Home from "./Components/Pages/Home/Home.tsx";
 import Cart from "./Components/Pages/Cart/Cart.tsx";
 import NavbarComponent from "./Components/Utility/Navbar/Navbar.tsx";
@@ -8,24 +8,22 @@ import NavbarComponent from "./Components/Utility/Navbar/Navbar.tsx";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: (
-            <Home />
-        ),
-    },
-    {
-        path: "cart",
-        element: <Cart />,
-    },
+        element: <><NavbarComponent/> <Outlet /> </>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>,
+            },
+            {
+                path: '/cart',
+                element: <Cart/>,
+            },
+        ]
+    }
 ]);
 
 function App() {
-
-  return (
-    <>
-        <NavbarComponent />
-        <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- Exercise/React/react-product-listing-exercise/src/Components/Utility/Navbar/Navbar.tsx
+++ Exercise/React/react-product-listing-exercise/src/Components/Utility/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from "reactstrap";
+import {Nav, Navbar, NavbarBrand, NavItem, } from "reactstrap";
+import {NavLink} from "react-router-dom";
 
 export default function NavbarComponent() {
     return (
@@ -6,10 +7,12 @@ export default function NavbarComponent() {
             <NavbarBrand href="/">Product Listing</NavbarBrand>
             <Nav className="ms-auto" navbar>
                 <NavItem>
-                    <NavLink href="/" active>Home</NavLink>
+                    {/*<NavLink href="/" active>Home</NavLink>*/}
+                    <NavLink to="/" className="nav-link">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink href="/cart">Cart</NavLink>
+                    {/*<NavLink href="/cart">Cart</NavLink>*/}
+                    <NavLink to="/cart" className="nav-link">Cart</NavLink>
                 </NavItem>
             </Nav>
         </Navbar>
