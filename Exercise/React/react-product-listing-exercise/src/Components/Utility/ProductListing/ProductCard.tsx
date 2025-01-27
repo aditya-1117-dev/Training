07ac128eae6550fb65d455910a71b5d62f9f9d46
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-27 16:25:58 +0530

    Feat add single product card

---
+++ Exercise/React/react-product-listing-exercise/src/Components/Utility/ProductListing/ProductCard.tsx
@@ -0,0 +1,34 @@
+import {Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Row} from "reactstrap";
+import {IProduct} from "../../Types/UtilityTypes.tsx";
+
+export default function ProductCard({product}:{product: IProduct}){
+
+    return (
+        <Card key={product.id} className="mb-3">
+            <Row className="g-0 align-items-center">
+                <Col md={2}>
+                    <CardImg src={product.thumbnail} alt={product.title} className="img-fluid"/>
+                </Col>
+                <Col md={8}>
+                    <CardBody>
+                        <CardTitle tag="h5">{product.title}</CardTitle>
+                        <CardText>
+                            <small className="text-muted">{product.category}</small>
+                        </CardText>
+                        <CardText>
+                            {product.description.slice(0, 90)}{product.description.length > 90 ? "..." : ""}
+                        </CardText>
+                    </CardBody>
+                </Col>
+                <Col md={2}>
+                    <CardBody>
+                        <CardText>
+                            <strong>${(product.price - (product.price * product.discountPercentage) / 100).toFixed(3)}</strong>{" "}<s>(${product.price})</s>
+                        </CardText>
+                        <Button color="primary">Add to Cart</Button>
+                    </CardBody>
+                </Col>
+            </Row>
+        </Card>
+    )
+}
