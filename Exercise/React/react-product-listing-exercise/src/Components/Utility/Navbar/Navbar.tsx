273e68a28838bb094ea787934f61718a89762163
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-01-30 20:38:22 +0530

    Feat add user dropdown in navbar

--- Exercise/React/react-product-listing-exercise/src/Components/Utility/Navbar/Navbar.tsx
+++ Exercise/React/react-product-listing-exercise/src/Components/Utility/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 import {Nav, Navbar, NavbarBrand, NavItem } from "reactstrap";
 import {NavLink} from "react-router-dom";
+import DropdownComponent from "../Dropdown/Dropdown.tsx";
+import {useContext} from "react";
+import { UserContext } from "../../../App.tsx";
 
-export default function NavbarComponent() {
+export default function NavbarComponent({users}) {
+    const [currentUser, setCurrentUser] = useContext(UserContext);
+    const usernames = users?.data?.users.map(user => user.username)
+
     return (
         <Navbar color="dark" light={false} dark={true} fixed="top" expand="md" className="mb-4">
             <NavbarBrand href="/">Product Listing</NavbarBrand>
@@ -12,6 +18,12 @@ export default function NavbarComponent() {
                 <NavItem>
                     <NavLink to="/cart" className="nav-link">Cart</NavLink>
                 </NavItem>
+
+                <DropdownComponent
+                    baseValue="Select the User"
+                    list={usernames}
+                    selectedItem={currentUser}
+                    setSelectedItem={setCurrentUser} />
             </Nav>
         </Navbar>
     )
