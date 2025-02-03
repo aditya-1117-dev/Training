8bcad2f1ac8d36d2f1fa4db9d8e49aeb2b226721
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-03 12:52:10 +0530

    Feat create functions for nested logic

--- Exercise/React/react-product-listing-exercise/src/Components/Pages/Home/Home.tsx
+++ Exercise/React/react-product-listing-exercise/src/Components/Pages/Home/Home.tsx
@@ -3,62 +3,62 @@ import {ChangeEvent, FC, useState} from "react";
 import "./Home.css";
 import useFetch from "../../Utility/CustomHooks/fetchData.tsx";
 import ListProducts from "../../Utility/ProductListing/ListProducts.tsx";
-import LoadingComponent from "../../Utility/Loader/Spinner.tsx";
 import {IFetchedCategories, IFetchedProducts, IProduct, IuseFromInput} from "../../Types/UtilityTypes.tsx";
 import {Container, Input} from "reactstrap";
-import DropdownComponent from "../../Utility/Dropdown/Dropdown.tsx";
+import Loader from "../../Utility/Loader/Loader.tsx";
+import DropdownItems from "../../Utility/Dropdown/DropdownItems.tsx";
 
 const baseUrl = 'https://dummyjson.com/products';
+
+function findUrl(query : string, category : string, limitAndSkip? : string) {
+    return query !== ""
+        ? category !==""? `${baseUrl}${query}&limit=0&skip=0`
+            : `${baseUrl}${query}&${limitAndSkip}`
+        : (category === ""
+            ? `${baseUrl}?${limitAndSkip}`
+            : `${baseUrl}/category/${category}`) ;
+}
+
+function filterByKey( array1 : IProduct[] | undefined, key: keyof IProduct, value : string) {
+    return ( value==="" )
+        ? array1
+        : array1?.filter((product : IProduct )=> product[key] === value);
+}
 
 const Home : FC = () =>{
     const categories : IFetchedCategories = useFetch(`https://dummyjson.com/products/category-list`);
     const [selectedCategory, setSelectedCategory] = useState<string>("");
-
     const search : IuseFromInput  = useFormInputBox("");
-
     const query : string = search.value.length > 0 ? `/search?q=${search.value}` : '';
 
     const [currentPage, setCurrentPage] = useState(1);
-    const [limit, setLimit] = useState("20");
-    const limitAndSkip = `limit=${limit}&skip=${(currentPage-1)*Number(limit)}`;
+    const [limit, setLimit] = useState(20);
+    const limitAndSkip = `limit=${limit}&skip=${(currentPage-1)*limit}`;
+    const url = findUrl(query, selectedCategory, limitAndSkip);
+    const paginatedProducts : IFetchedProducts = useFetch(url, query !== ""? 1000 : 0);
+    const totalPages = Math.ceil( (paginatedProducts?.data?.total?? 0) / limit );
 
-    const url = query !== ""
-        ? selectedCategory !==""? `${baseUrl}${query}`
-            : `${baseUrl}${query}&${limitAndSkip}`
-        : (selectedCategory === ""
-            ? `${baseUrl}?${limitAndSkip}`
-            : `${baseUrl}/category/${selectedCategory}`) ;
-
-    const paginatedProducts : IFetchedProducts = useFetch(url, query !== ""? 3000 : 0);
-
-    const totalPages = Math.ceil( (paginatedProducts?.data?.total?? 0) / Number(limit) );
-
-    // limit=n,skip=0 - filter
-
-    const showProducts : IProduct[] | undefined = ( selectedCategory==="" )
-        ? paginatedProducts?.data?.products
-        : paginatedProducts?.data?.products?.filter((product : IProduct )=> product.category ===selectedCategory);
-
-
+    const products : IProduct[] | undefined = filterByKey( paginatedProducts?.data?.products, "category", selectedCategory);
     function handleInputChange(e : ChangeEvent<HTMLInputElement>) {
         search.onChange(e);
-        setCurrentPage(1)
+        setCurrentPage(1);
     }
-
     return (
         <>
             <Container className="search-container">
                 <Input className="search-input" placeholder="Search for products" value={search.value} onChange={handleInputChange} />
-                <DropdownComponent baseValue="Select the Category" list={categories?.data} selectedItem={selectedCategory} setSelectedItem={setSelectedCategory} />
+                <DropdownItems baseValue="Select the Category" list={categories?.data} selectedItem={selectedCategory} setSelectedItem={setSelectedCategory} />
             </Container>
             {paginatedProducts?.loading
-                ? <LoadingComponent width={100} height={100}/>
+                ? <Loader width={100} height={100}/>
                 : <ListProducts
-                    showProducts={showProducts}
-                    totalPages={totalPages}
+                    products={products}
+                    totalPages={selectedCategory !== ""
+                        ? Math.ceil( (products?.length?? 0) / limit)
+                        : totalPages}
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
-                    limit={Number(limit)}
+                    limit={limit}
                     setLimit={setLimit}
                     loading={paginatedProducts?.loading}
                 />
