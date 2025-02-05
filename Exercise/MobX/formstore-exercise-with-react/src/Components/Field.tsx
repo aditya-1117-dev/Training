4e0349c16ef024f7db8f9b6678ee48bcffbdc31a
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-05 17:40:46 +0530

    Feat add Field Component

---
+++ Exercise/MobX/formstore-exercise-with-react/src/Components/Field.tsx
@@ -0,0 +1,46 @@
+import {FormStore} from "../Stores/formStore.tsx";
+import {ChangeEvent, useContext} from "react";
+import {observer} from "mobx-react-lite";
+import {Col, FormGroup, Label} from "reactstrap";
+import ReactInput from "./ReactInput.tsx";
+import {FormStoreContext} from "../Context/FormContext.tsx";
+
+interface IField {
+    type : 'text'|'number',
+    name : string,
+    label : string,
+    store : FormStore,
+    required : boolean,
+}
+
+function Field({ type, name, label, store, required} : IField){
+    const contextStore = useContext(FormStoreContext);
+    const formStore = store || contextStore;
+
+    const value = formStore.getValue(name);
+    const disabled = formStore.isSubmitted ;
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>, key : string) => {
+        formStore.setValue(key, e.target.value);
+    };
+
+    return (
+        <FormGroup row>
+            <Label for={name} sm={4}>
+                {label}{required && <span style={{color: "red"}}>*</span>}
+            </Label>
+            <Col sm={8}>
+                <ReactInput
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={handleChange}
+                    disabled={disabled}
+                    required={required}
+                />
+            </Col>
+        </FormGroup>
+    )
+}
+
+export default observer(Field);
