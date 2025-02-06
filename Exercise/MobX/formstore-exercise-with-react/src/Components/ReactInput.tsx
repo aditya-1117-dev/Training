e6e947b386ade1e10c631d102208b02e2eb628e9
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-06 10:52:09 +0530

    Refactor input component for type safety

--- Exercise/MobX/formstore-exercise-with-react/src/Components/ReactInput.tsx
+++ Exercise/MobX/formstore-exercise-with-react/src/Components/ReactInput.tsx
@@ -1,25 +1,28 @@
-import {observer} from "mobx-react-lite";
-import {Input} from "reactstrap";
-import {ChangeEvent} from "react";
+import {ChangeEvent} from 'react';
+import {observer} from 'mobx-react-lite';
+import {Input as ReactStrapInput} from 'reactstrap';
+import {IFormStore} from "../App.tsx";
 
-interface IReactInput {
-    type: 'text' | 'number',
-    name: string,
-    value : number | string
-    required: boolean,
-    disabled : boolean,
-    onChange : (e: ChangeEvent<HTMLInputElement>, key : string) => void,
+interface IInput {
+    type: 'text' | 'number';
+    name: keyof IFormStore;
+    value: number | string;
+    required: boolean;
+    disabled: boolean;
+    onChange: (e: ChangeEvent<HTMLInputElement>, key: keyof IFormStore) => void;
 }
 
-function ReactInput({ type, name, value, onChange, disabled, required } : IReactInput) {
+function Input({type, name, value, onChange, disabled, required}: IInput) {
     return (
-        <Input
+        <ReactStrapInput
             type={type}
             name={name}
             value={value}
             onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e, name)}
             disabled={disabled}
-            required={required} />
-    )
+            required={required}
+        />
+    );
 }
-export default observer(ReactInput);
+
+export default observer(Input);
