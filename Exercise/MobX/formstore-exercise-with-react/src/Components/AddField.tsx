eee7760da378a85526db912b0e32505e408442c7
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-10 22:12:34 +0530

    Feat add new input button for json fields

---
+++ Exercise/MobX/formstore-exercise-with-react/src/Components/AddField.tsx
@@ -0,0 +1,10 @@
+import {observer} from "mobx-react-lite";
+import {Button} from "reactstrap";
+
+function AddField({ handleAddNewInput,disabled }: { handleAddNewInput: Function, disabled?: boolean } ) {
+    return (
+            <Button onClick={() => handleAddNewInput()} disabled={disabled}> Add Field </Button>
+    );
+};
+
+export default observer(AddField);
