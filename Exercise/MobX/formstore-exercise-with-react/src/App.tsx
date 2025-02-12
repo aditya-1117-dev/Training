e85a7eb2c36f8c893595c495a30ee1c3cf789a0c
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-12 12:55:30 +0530

    Refactor routing components & remove extra Checkbox component

--- Exercise/MobX/formstore-exercise-with-react/src/App.tsx
+++ Exercise/MobX/formstore-exercise-with-react/src/App.tsx
@@ -1,55 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from "./Components/Form.tsx";
-import {CheckField, JSONField, NumberField, RadioField, SelectField, StringField} from "./Components/InputFields.tsx";
 import {observer} from "mobx-react-lite";
-import { ListTableStoreContext } from './Context/ListTableContext.tsx';
-import ListTable from "./Components/ListTable.tsx";
-import {formStore, IStoreData} from "./Context/FormContext.tsx";
-import ListTableStore from "./Stores/ListTableStore.tsx";
-import {IFetchedProducts} from "./types/tableType.tsx";
+import {formStore} from "./Context/FormContext.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-
-const baseUrl: string = `https://dummyjson.com/products`;
-export const listTableStore : ListTableStore<IFetchedProducts> = new ListTableStore<IFetchedProducts>(baseUrl);
+import ProductForm from "./Components/ProductForm.tsx";
+import ProductTable from "./Components/ProductTable.tsx";
 
 function App() {
-    // const columns = ["title","description", "price","thumbnail"];
-    const columns = {title: "Product Title", description: "Description", price: "Price", thumbnail: "Image"};
-
     return (
         <BrowserRouter>
-            <Routes >
-                <Route path="/" element={(
-                    <Form showResetButton={true} showSaveButton={true} formStore={formStore}>
-                        <JSONField name="jsonStringField" required={true}
-                                   renderField={(value: string, index: number, name: keyof IStoreData, required: boolean) => (
-                                       <StringField name={name} label={index === 0 ? "String Input" : ""} index={index}
-                                                    required={required} value={value} key={index}/>)}/>
-
-                        <JSONField name="jsonNumberField" required={true}
-                                   renderField={(value: number, index: number, name: keyof IStoreData, required: boolean) => (
-                                       <NumberField name={name} label={index === 0 ? "Number Input" : ""} index={index}
-                                                    required={required} value={value} key={index}/>)}/>
-
-                        <StringField name="firstName" label="First Name" required={true} min={2} max={5}/>
-                        <NumberField name="age" label="Your Age" required={false} min={2} max={5}/>
-                        <SelectField label="Select Input :" name="select" required={true}
-                                     options={[{value: 'null', label: 'Select the Value'}, {
-                                         value: 'abc',
-                                         label: 'abc'
-                                     }, {value: 'bcd', label: 'bcd'}, {value: 'efg', label: 'efg'}]}/>
-                        <RadioField label="Gender" name="gender" required={true}
-                                    options={[{value: 'Male', label: 'Male'}, {value: 'Female', label: 'Female'}]}/>
-                        <CheckField label="country" name="country" required={true}
-                                    options={[{value: 'IND', label: 'IND'}, {value: 'UK', label: 'UK'}]}/>
-                    </Form>
-                )} />
-                <Route path={"/product-table"} element={(
-                    <ListTableStoreContext.Provider value={listTableStore}>
-                        <ListTable columns={columns} Key={"products"}/>
-                    </ListTableStoreContext.Provider>
-                )} />
+            <Routes>
+                <Route path="/" element={ <ProductForm formStore={formStore} />}/>
+                <Route path={"/product-table"} element={<ProductTable/> }/>
             </Routes>
         </BrowserRouter>
     )
--- Exercise/MobX/formstore-exercise-with-react/src/Components/ListTable.tsx
+++ Exercise/MobX/formstore-exercise-with-react/src/Components/ListTable.tsx
@@ -1,52 +1,58 @@
-import { observer } from 'mobx-react-lite';
+import {observer} from 'mobx-react-lite';
 import {Col, Container, Input, Row, Table} from "reactstrap";
 import PaginationComponent from "./Pagination/Pagination.tsx";
 import {ChangeEvent, useContext} from "react";
 import {ListTableStoreContext} from "../Context/ListTableContext.tsx";
 import Loader from "./Loader.tsx";
 import ListRows from "./ListRows.tsx";
-import CheckBox from "./CheckBox.tsx";
+import Checkbox from "./InputFields/Checkbox.tsx";
 
-const ListTable = ({ columns, Key } : any) => {
+const ListTable = ({columns, name}: any) => {
     const store = useContext(ListTableStoreContext);
-    const tableContent =  store.getKeyInData(Key);
+    const tableContent = store.getKeyInData(name);
     const selectAll = store.isSelectAll();
 
-    function onCheckboxSelection( id: number){
+    function onCheckboxSelection(id: number) {
         return store.updateSelection(id);
     }
+
     function handleSelectAll() {
-        !store.isSelectAll()? store.selectAll() : store.deSelectAll();
+        !store.isSelectAll() ? store.selectAll() : store.deSelectAll();
     }
+
     return (
         <Col>
             <Container className="search-container">
-                <Input className="search-input" placeholder="Search for products" value={store.searchQuery} onChange={(e : ChangeEvent<HTMLInputElement>)=> store.setSearchQuery(e.target.value)} />
+                <Input className="search-input" placeholder="Search for products" value={store.searchQuery}
+                       onChange={(e: ChangeEvent<HTMLInputElement>) => store.setSearchQuery(e.target.value)}/>
             </Container>
-            <Col md={8} style={{ margin: "0 auto" }}>
-                {store.isLoading()? <Loader height={100} width={100} /> :
-                    tableContent.length === 0 ? <Row className="bold justify-content-center"> No Products Available</Row> :
+            <Col md={8} style={{margin: "0 auto"}}>
+                {store.isLoading() ? <Loader height={100} width={100}/> :
+                    tableContent.length === 0 ?
+                        <Row className="bold justify-content-center"> No Products Available</Row> :
                         (
                             <Table bordered hover responsive>
                                 <thead>
                                 <tr>
                                     <th className="text-uppercase text-center">
-                                        <CheckBox value={selectAll} onchange={(handleSelectAll)} />
+                                        <Checkbox value={selectAll} onChange={handleSelectAll} name="name"
+                                                  options={[{value: '', label: ''}]}/>
                                     </th>
-                                    {Object.keys(columns).map((colKey: string) => (
-                                        <td key={colKey} className="text-center">{columns[colKey]}</td>
+                                    {columns.map((column : any) => (
+                                        <td key={column.name} className="text-center">{column.display}</td>
                                     ))}
                                 </tr>
                                 </thead>
                                 <tbody>
                                 {tableContent.map((row: any, index: number) => (
-                                        <tr key={row.id}>
-                                            <td className="text-center">
-                                                <CheckBox id={index} value={store.isSelected(index)}
-                                                          onchange={() => onCheckboxSelection(index)}/>
-                                            </td>
-                                            <ListRows row={row} columns={Object.keys(columns)}/>
-                                        </tr>
+                                    <tr key={row.id}>
+                                        <td className="text-center">
+                                            <Checkbox value={store.isSelected(index)}
+                                                      onChange={() => onCheckboxSelection(index)} name="name"
+                                                      options={[{value: '', label: ''}]}/>
+                                        </td>
+                                        <ListRows row={row} columns={columns.map((column : any)=> column.name)}/>
+                                    </tr>
                                 ))}
                                 </tbody>
                             </Table>
