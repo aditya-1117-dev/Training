24680c7ae6c550b83582b940f2aecefd20a31956
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-12 12:58:43 +0530

    Refactor contexts of FormStore & TableStore

--- Exercise/MobX/formstore-exercise-with-react/src/Context/FormContext.tsx
+++ Exercise/MobX/formstore-exercise-with-react/src/Context/FormContext.tsx
@@ -2,29 +2,27 @@ import {FormStore} from "../Stores/formStore.tsx";
 import {createContext} from "react";
 
 
-export interface IStoreData {
+export interface IProductData {
     firstName: string,
     name: string,
     age: number,
     country: string[],
     select: "abc" | "bcd" | "efg",
     gender: "Male" | "Female",
-    jsonObjectInput : [string[], "Male" | "Female", string, number, "first" | "second" | "third"],
-    jsonStringField : string[],
-    jsonNumberField : number[]
+    stringField : string[],
+    numberField : number[],
 }
-const FormData : IStoreData = {
+const FormData : IProductData = {
     firstName: "A",
     name: "Adi",
     age: 10,
     select: "efg",
     gender: "Male",
     country: ["IND"],
-    jsonObjectInput : [["IND"],"Male","Adi", 1, "first",],
-    jsonStringField : ["a", "b"],
-    jsonNumberField : [1, 2, 3, 4],
+    stringField : ["a", "b"],
+    numberField : [1, 2, 3, 4],
 }
-export const formStore = new FormStore<IStoreData>(FormData);
-formStore.setOnSubmitCallBack(() => console.log("on submit call"));
+export const formStore = new FormStore<IProductData>(FormData);
+formStore.setOnSubmitCallBack(() => {});
 
-export const FormStoreContext = createContext<FormStore<IStoreData>>(formStore);
+export const FormStoreContext = createContext<FormStore<IProductData>>(formStore);
--- Exercise/MobX/formstore-exercise-with-react/src/Context/ListTableContext.tsx
+++ Exercise/MobX/formstore-exercise-with-react/src/Context/ListTableContext.tsx
@@ -2,6 +2,7 @@ import {createContext} from "react";
 import ListTableStore from "../Stores/ListTableStore.tsx";
 import {IFetchedProducts} from "../types/tableType.tsx";
 
-const listTableStore : ListTableStore<IFetchedProducts> = new ListTableStore<IFetchedProducts>();
+const baseUrl: string = ``;
+export const listTableStore : ListTableStore<IFetchedProducts> = new ListTableStore<IFetchedProducts>(baseUrl);
 
 export const ListTableStoreContext = createContext<ListTableStore<IFetchedProducts>>(listTableStore);
