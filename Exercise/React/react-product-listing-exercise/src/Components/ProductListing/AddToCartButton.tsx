dee26f9bba10aa50f9f585f745c883df33d984f9
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-25 17:40:19 +0530

    Feat hide add to cart button for moderator

--- Exercise/React/react-product-listing-exercise/src/Components/ProductListing/AddToCartButton.tsx
+++ Exercise/React/react-product-listing-exercise/src/Components/ProductListing/AddToCartButton.tsx
@@ -1,20 +1,23 @@
 import {Button, ButtonGroup, Col} from "reactstrap";
 import {MouseEventHandler} from "react";
 export default function AddToCartButton({quantity, handleAddToCart, handleDecreaseFromCart, handleRemoveItemFromCart} : { quantity : number, handleAddToCart : MouseEventHandler, handleDecreaseFromCart : MouseEventHandler, handleRemoveItemFromCart : MouseEventHandler}){
+    const userRole = localStorage.getItem("role");
     return(
         <>
-            {quantity > 0  ? (
-                <Col>
-                    <ButtonGroup>
-                        <Button color="danger" onClick={handleDecreaseFromCart} size="sm">-</Button>
-                        <Button color="primary" outline disabled> {quantity} </Button>
-                        <Button color="success" onClick={handleAddToCart} size="sm">+</Button>
-                    </ButtonGroup>
-                    <Button color="danger" onClick={handleRemoveItemFromCart} className="mt-1" size="sm">Remove Item</Button>
-                </Col>
-            ) : (
-                <Button onClick={handleAddToCart} color="primary">Add to Cart</Button>
-            )}
+            {userRole !== 'moderator' &&
+                (quantity > 0  ? (
+                    <Col>
+                        <ButtonGroup>
+                            <Button color="danger" onClick={handleDecreaseFromCart} size="sm">-</Button>
+                            <Button color="primary" outline disabled> {quantity} </Button>
+                            <Button color="success" onClick={handleAddToCart} size="sm">+</Button>
+                        </ButtonGroup>
+                        <Button color="danger" onClick={handleRemoveItemFromCart} className="mt-1" size="sm">Remove Item</Button>
+                    </Col>
+                ) : (
+                    <Button onClick={handleAddToCart} color="primary">Add to Cart</Button>
+                ) )
+            }
         </>
     )
 }
