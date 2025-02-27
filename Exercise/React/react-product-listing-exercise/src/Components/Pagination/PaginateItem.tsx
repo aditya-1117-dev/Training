0a2439cdb71f34771d268dc881ee0764a23ea019
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-02-27 15:57:54 +0530

    Fix nested conditional rendering

--- Exercise/React/react-product-listing-exercise/src/Components/Pagination/PaginateItem.tsx
+++ Exercise/React/react-product-listing-exercise/src/Components/Pagination/PaginateItem.tsx
@@ -1,19 +1,28 @@
 import {PaginationItem, PaginationLink} from "reactstrap";
 import {MouseEventHandler} from "react";
 
-export default function PaginateItem({value, onClick, disabled=false, active=false, linkProp = ""} : {value? : string | number, onClick : MouseEventHandler, disabled? : boolean, active? : boolean, linkProp? : "" | "first" | "previous" | "next" | "last" }) {
+export default function PaginateItem({value, onClick, disabled=false, active=false, linkProp = ""}
+                                         : {value? : string | number, onClick : MouseEventHandler, disabled? : boolean, active? : boolean, linkProp? : "" | "first" | "previous" | "next" | "last" }) {
+    let paginationLink = null;
+    switch (linkProp) {
+        case "first":
+            paginationLink = <PaginationLink first onClick={onClick} />;
+            break;
+        case "previous":
+            paginationLink = <PaginationLink previous onClick={onClick} />;
+            break;
+        case "next":
+            paginationLink = <PaginationLink next onClick={onClick} />;
+            break;
+        case "last":
+            paginationLink = <PaginationLink last onClick={onClick} />;
+            break;
+        default:
+            paginationLink = <PaginationLink onClick={onClick}>{value}</PaginationLink>;
+    }
     return (
         <PaginationItem disabled={disabled} active={active}>
-            {linkProp === ""
-                ? <PaginationLink onClick={onClick}> {value}</PaginationLink>
-                : linkProp === "first"
-                    ? <PaginationLink first onClick={onClick} />
-                    : linkProp === "previous"
-                        ? <PaginationLink previous onClick={onClick} />
-                        : linkProp === "next"
-                            ? <PaginationLink next onClick={onClick} />
-                            : <PaginationLink last onClick={onClick} /> }
-
+            {paginationLink }
         </PaginationItem>
     )
 }
