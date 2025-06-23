694cc708624e1853b1158ec7c72795530275f16d
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-06-23 18:21:22 +0530

    Feat : add useAuth hook

---
+++ issue_tracker_system/frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,10 @@
+import {useContext} from "react";
+import {AuthContext, type IAuthContext} from "../context/auth/AuthContext.tsx";
+
+export const useAuth = () : IAuthContext => {
+    const context = useContext<IAuthContext | null>(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
