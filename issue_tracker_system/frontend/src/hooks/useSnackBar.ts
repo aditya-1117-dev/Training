4b1725fa01be4106fc64663c2ae9072a2a914875
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-06-30 13:59:42 +0530

    Feat : add useSnackBar hook

---
+++ issue_tracker_system/frontend/src/hooks/useSnackBar.ts
@@ -0,0 +1,10 @@
+import {useContext} from "react";
+import {SnackbarContext} from "../context/snackBar/SnackBarContext.tsx";
+
+export const useSnackbar = () => {
+    const context = useContext(SnackbarContext);
+    if (!context) {
+        throw new Error('useSnackbar must be used within a SnackbarProvider');
+    }
+    return context;
+};
