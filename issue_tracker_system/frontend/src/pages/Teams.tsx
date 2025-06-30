dc7c4cf633ba52bfc49abb36ecba72c513f95137
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-06-30 14:50:31 +0530

    Feat : update teams page for using useAPI

--- issue_tracker_system/frontend/src/pages/Teams.tsx
+++ issue_tracker_system/frontend/src/pages/Teams.tsx
@@ -1,137 +1,148 @@
-import {useState, useEffect, type ReactNode} from 'react';
-import {Box, Button, Typography, Alert, Stack, Paper, Snackbar} from '@mui/material';
-import {GroupAdd} from '@mui/icons-material';
-import {getRequest, postRequest} from "../utils/apiClient.tsx";
-import {RenderTable} from '../components/RenderTable.tsx';
-import {useAuth} from "../hooks/useAuth.tsx";
-import {CreateTeamDialog} from "../components/CreateTeamDialog.tsx";
-import type {ITeam, ITeamCreateData} from "../types/teamTypes.tsx";
-import {useNavigate} from "react-router-dom";
-import type {IUser} from "../types/userTypes.tsx";
+import {Box, Button, Typography, Stack, Paper, IconButton} from '@mui/material';
+import {Delete, Edit, GroupAdd} from '@mui/icons-material';
+import {type IColumn, RenderTable} from '../components/table/RenderTable.tsx';
+import {CreateTeamDialog} from '../components/CreateTeamDialog.tsx';
+import type {ITeam, ITeamCreateData, ITeamUpdateData} from '../types/team.ts';
+import {type FC, useState} from "react";
+import {EditTeamDialog} from "../components/EditTeamDialog.tsx";
+import {useSnackbar} from "../hooks/useSnackBar.ts";
+import type {IUser, IUserUpdateData} from "../types/user.ts";
+import {useAPI} from "../hooks/useAPI.ts";
 
-export default function Teams() {
-    const [teams, setTeams] = useState<ITeam[]>([]);
-    const [users, setUsers] = useState<IUser[]>([]);
+const Teams: FC = () => {
+    const {addSnackbar} = useSnackbar();
+    const [openEditTeamDialog, setOpenEditTeamDialog] = useState<boolean>(false);
+    const [selectedTeam, setSelectedTeam] = useState<ITeam | null>(null);
     const [openCreateTeamsDialog, setOpenCreateTeamsDialog] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const {token} = useAuth();
-    const navigate = useNavigate();
-    const [snackbar, setSnackbar] = useState<{ open: boolean; message: string; severity: 'success' | 'error' }>({
-        open: false,
-        message: '',
-        severity: 'error',
+    const {data: teams, isLoading: teamsLoading, execute: fetchTeams} = useAPI<ITeam[]>('/api/teams', {method: "GET"})
+    const {data: users, execute: fetchUsers} = useAPI<IUser[]>('/api/users', {method: "GET"})
+
+    const {execute: handleDeleteTeam} = useAPI<IUser>('/api/teams/:id', {
+        method: 'DELETE',
+        onSuccess: () => {
+            if (fetchTeams) {
+                fetchTeams();
+            }
+            addSnackbar( { severity : 'success', message : 'User deleted successfully!'})
+        },
+        onError: (err: unknown) => {
+            addSnackbar( { severity : 'error', message : err instanceof Error ? err.message : 'Delete operation failed'})
+        }
+    });
+    const {execute: updateTeam, isLoading: teamUpdateLoading} = useAPI<IUser, ITeamUpdateData>('/api/teams/:id', {
+        method: 'PUT',
+        onSuccess: () => {
+            setOpenEditTeamDialog(false);
+            setSelectedTeam(null);
+            if (fetchTeams) {
+                fetchTeams();
+            }
+            addSnackbar( { severity : 'success', message : 'Team updated successfully!'})
+        },
+        onError: (err: unknown) => {
+            addSnackbar( { severity : 'error', message : err instanceof Error ? err.message : 'Team update failed'})
+        }
     });
 
-    const columns: {
-        key: string; header: string; render?: (item: any) => ReactNode;
-        align?: 'left' | 'center' | 'right'; width?: string | number;
-    }[] = [
-        {key: 'name', header: 'Name'},
-        {key: 'description', header: 'Description'},
-        {key: 'team_lead_name', header: 'Team Lead'},
-        {key: 'member_count', header: 'Members', align: 'center'},
-        {key: 'created_at', header: 'Created At', render: team => new Date(team.created_at).toLocaleString()}
+    const {execute: updateUser, isLoading: userUpdateLoading} = useAPI<IUser, IUserUpdateData>('/api/users/:id', {
+        method: 'PUT',
+        onError: (err: unknown) => {
+            addSnackbar( { severity : 'error', message : err instanceof Error ? err.message : 'Update operation failed'})
+        },
+    });
+
+    const {execute: postNewTeam, isLoading: postNewTeamLoading} = useAPI<ITeam, ITeamCreateData>('/api/teams', {
+        method: "POST",
+        onSuccess: () => {
+            if (fetchTeams) fetchTeams();
+            if (fetchUsers) fetchUsers();
+            addSnackbar( { severity : 'success', message : 'Team created successfully!'})
+        },
+        onError: (err: unknown) => {
+            addSnackbar( { severity : 'error', message : err instanceof Error ? err.message : 'Failed to create team'})
+        },
+    })
+
+    const createNewTeam = async (data: ITeamCreateData, memberID: string) => {
+        try {
+            await updateUser({body: {role: 'TEAM_LEAD'}, pathParams: {id: memberID}});
+
+            await postNewTeam({body: data});
+        } catch (err: unknown) {
+            addSnackbar( { severity : 'error', message : err instanceof Error ? err.message : 'Failed to create team'})
+        }
+    };
+
+    const handleEditTeam = (team: ITeam | null) => {
+        setSelectedTeam(team);
+        setOpenEditTeamDialog(openEditTeamDialog => !openEditTeamDialog);
+    };
+
+    const soloMembers = users?.filter((user: IUser) => user.role === 'MEMBER' && user.team_id == null) || [];
+
+    const soloMembersWithinCurrentTeam = [...soloMembers, ...(selectedTeam ? users?.filter((user: IUser) => user.team_id === selectedTeam.id) || [] : [])];
+
+    const renderRowNumber = (index: number = 0) => index + 1;
+
+    const columns: IColumn<ITeam>[] = [
+        {key: 'row_number', label: 'ID', width: '5%', render: (_, index) => renderRowNumber(index)},
+        {key: 'name', label: 'Name'},
+        {key: 'description', label: 'Description'},
+        {key: 'team_lead_name', label: 'Team Lead'},
+        {key: 'member_count', label: 'Members', align: 'center'},
+        {key: 'created_at', label: 'Created At', render: team => new Date(team.created_at).toLocaleString()},
+        {
+            key: 'actions', label: 'Actions', width: '15%', align: 'center',
+            render: (team: ITeam) => (
+                <Stack direction="row" spacing={1} justifyContent="center">
+                    <IconButton color="primary" onClick={() => handleEditTeam(team)}
+                                aria-label={`Edit user ${team.name}`}>
+                        <Edit/>
+                    </IconButton>
+                    <IconButton color="error" onClick={() => handleDeleteTeam({pathParams: {id: team.id}})}>
+                        <Delete/>
+                    </IconButton>
+                </Stack>
+            ),
+        },
     ];
-
-    useEffect(() => {
-        fetchTeams();
-        fetchTeamLeads()
-    }, []);
-
-    const fetchTeamLeads = async () => {
-        try {
-            const res = await getRequest<IUser[]>('http://localhost:3000/api/users', {
-                Authorization: `Bearer ${token}`
-            });
-            if (res.data) {
-                const data = res.data.filter(user => {
-                    return user.role==="TEAM_LEAD";
-                })
-                setUsers(data);
-            }
-        } catch (err : unknown) {
-            setUsers([]);
-        }
-    };
-
-    const fetchTeams = async () => {
-        setLoading(true);
-        try {
-            const response = await getRequest<ITeam[]>('http://localhost:3000/api/teams', {
-                Authorization: `Bearer ${token}`
-            });
-            if (response.data) setTeams(response.data);
-        } catch (err: unknown) {
-            setSnackbar({
-                open: true,
-                message: err instanceof Error ? err.message : 'Failed to load teams',
-                severity: 'error'
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleCreate = async (data: ITeamCreateData) => {
-        try {
-            const response = await postRequest<{
-                data: ITeam
-            }, ITeamCreateData>('http://localhost:3000/api/teams', data, {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            });
-            if (response.error) throw new Error("Team creation failed");
-            await fetchTeams();
-            setSnackbar({ open: true, message: 'Team created successfully', severity: 'success' });
-        } catch (err: unknown) {
-            setSnackbar({
-                open: true,
-                message: err instanceof Error ? err.message : 'Failed to create team',
-                severity: 'error'
-            });
-        }
-    };
 
     return (
         <Box sx={{width: '100%', py: 4, px: {xs: 2, sm: 4, md: 6}, boxSizing: 'border-box'}}>
             <Paper sx={{p: 3, overflowX: 'auto'}}>
-                <Snackbar
-                    open={snackbar.open}
-                    autoHideDuration={3000}
-                    onClose={() => setSnackbar({ ...snackbar, open: false })}
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                >
-                    <Alert severity={snackbar.severity} onClose={() => setSnackbar({ ...snackbar, open: false })} sx={{ width: '100%' }}>
-                        {snackbar.message}
-                    </Alert>
-                </Snackbar>
-
-                <Stack
-                    direction={{xs: 'column', sm: 'row'}}
-                    justifyContent="space-between"
-                    alignItems="center"
-                    mb={3}
-                >
+                <Stack direction={{xs: 'column', sm: 'row'}} justifyContent="space-between" alignItems="center" mb={3}>
                     <Typography variant="h5">Team Management</Typography>
-                    <Box sx={{ display: 'flex', flexDirection: {xs: 'column', sm: 'row'}, gap: 1 }} >
-                        <Button variant="outlined" sx={{mr: 1}} onClick={()=> navigate('/users')}>
-                            Click here to move to Users
-                        </Button>
-                        <Button variant="contained" startIcon={<GroupAdd/>} onClick={() => setOpenCreateTeamsDialog(true)}>
-                            New Team
-                        </Button>
-                    </Box>
+                    <Button variant="contained" startIcon={<GroupAdd/>} onClick={() => setOpenCreateTeamsDialog(true)}>
+                        New Team
+                    </Button>
                 </Stack>
 
-                <RenderTable data={teams} columns={columns} loading={loading}/>
+                <RenderTable data={teams || []} columns={columns} loading={teamsLoading}/>
+
+                <EditTeamDialog
+                    open={openEditTeamDialog}
+                    onClose={() => {
+                        setOpenEditTeamDialog(false)
+                        setSelectedTeam(null);
+                    }}
+                    onSubmit={async (teamId: string, data: ITeamUpdateData) => updateTeam({
+                        pathParams: {id: teamId},
+                        body: data
+                    })}
+                    team={selectedTeam}
+                    users={soloMembersWithinCurrentTeam}
+                    loading={teamUpdateLoading}
+                />
 
                 <CreateTeamDialog
                     open={openCreateTeamsDialog}
                     onClose={() => setOpenCreateTeamsDialog(false)}
-                    onSubmit={handleCreate}
-                    users={users}
+                    onSubmit={createNewTeam}
+                    users={soloMembers}
+                    loading={userUpdateLoading || postNewTeamLoading}
                 />
             </Paper>
         </Box>
     );
 }
+export default Teams;
--- issue_tracker_system/frontend/src/types/team.ts
+++ issue_tracker_system/frontend/src/types/team.ts
@@ -6,6 +6,15 @@ export interface ITeam {
     team_lead_name: string;
     member_count: number;
     created_at: string;
+    is_active?: boolean;
+    updated_at?: string;
+}
+
+export interface ITeamUpdateData {
+    name?: string;
+    description?: string;
+    team_lead_id?: string;
+    is_active?: boolean;
 }
 
 export interface ITeamCreateData {
