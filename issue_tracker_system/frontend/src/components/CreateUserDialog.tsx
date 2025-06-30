8e1a11348be48fbe78b3cf1a8cd816cd46e553e1
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-06-30 14:47:31 +0530

    Feat : add create user component

--- issue_tracker_system/frontend/src/components/CreateUserDialog.tsx
+++ issue_tracker_system/frontend/src/components/CreateUserDialog.tsx
@@ -1,152 +1,140 @@
-import { useState } from 'react';
 import {
-    Dialog, DialogTitle, DialogContent, DialogActions,
-    TextField, Button, MenuItem, Stack, FormControl, InputLabel, Select, Alert, Snackbar
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    TextField,
+    Button,
+    MenuItem,
+    Stack,
+    FormControl,
+    InputLabel,
+    Select, type SelectChangeEvent
 } from '@mui/material';
-import type {IUserCreateData} from "../types/userTypes.tsx";
-import type {ITeam} from "../types/teamTypes.tsx";
+import type {IUserCreateData} from '../types/user.ts';
+import type {ITeam} from '../types/team.ts';
+import {useState} from "react";
+import {useSnackbar} from "../hooks/useSnackBar.ts";
 
 interface CreateUserDialogProps {
-    teams : ITeam[]
+    teams: ITeam[];
     open: boolean;
     onClose: () => void;
     onSubmit: (data: IUserCreateData) => Promise<void>;
+    loading?: boolean;
 }
 
-export function CreateUserDialog({ open, onClose, onSubmit, teams }: CreateUserDialogProps) {
+export function CreateUserDialog({open, onClose, onSubmit, teams, loading = false}: CreateUserDialogProps) {
     const [formData, setFormData] = useState<IUserCreateData>({
         name: '',
         email: '',
         password: '',
-        role: 'MEMBER'
+        role: 'MEMBER',
     });
-    const [loading, setLoading] = useState(false);
-    const [snackbar, setSnackbar] = useState<{ open: boolean; message: string; severity: 'success' | 'error' }>({
-        open: false,
-        message: '',
-        severity: 'error',
-    });
+    const {addSnackbar} = useSnackbar();
 
     const validateForm = (): string | null => {
-        if (!formData.name.trim()) return "Name is required";
-        if (formData.name.length < 2) return "Name must be at least 2 characters";
-        if (!/^[A-Za-z ]+$/.test(formData.name)) return "Name should only contain letters";
-        if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email)) return "Invalid email";
-        if (formData.password.length < 6) return "Password must be at least 6 characters";
+        if (!formData.name.trim()) return 'Name is required';
+        if (formData.name.length < 2) return 'Name must be at least 2 characters';
+        if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email)) return 'Invalid email';
+        if (formData.password.length < 6) return 'Password must be at least 6 characters';
         return null;
     };
 
-    const handleSubmit = async () => {
+    const createNewUser = async () => {
         const validationError = validateForm();
         if (validationError) {
-            setSnackbar({ open: true, message: validationError, severity: 'error' });
+            addSnackbar({severity: 'error', message: validationError})
             return;
         }
-        setLoading(true);
-        try {
-            await onSubmit(formData);
-            onClose();
+        await onSubmit(formData);
+        handleClose();
+    };
+
+    const handleRoleChange = (e: SelectChangeEvent) => {
+        if (e.target.value !== 'MEMBER' && formData.team_id) {
             setFormData({
-                name: '',
-                email: '',
-                password: '',
-                role: 'MEMBER'
-            })
-            setSnackbar({ open: true, message: 'User created successfully!', severity: 'success' });
-        } catch (err : unknown) {
-            setSnackbar({
-                open: true,
-                message: err instanceof Error ? err.message : 'Creation failed',
-                severity: 'error'
+                ...formData,
+                role: e.target.value as IUserCreateData['role'],
+                team_id: '',
             });
-        } finally {
-            setLoading(false);
+        } else {
+            setFormData({
+                ...formData,
+                role: e.target.value as IUserCreateData['role'],
+            });
         }
     };
 
+    const handleClose = () => {
+        setFormData({name: '', email: '', password: '', role: 'MEMBER'});
+        onClose();
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handleSelectChange = (e: SelectChangeEvent) => {
+        setFormData({...formData, team_id: e.target.value});
+    };
+
     return (
-        <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+        <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
             <DialogTitle>Create New User</DialogTitle>
             <DialogContent dividers>
-                <Snackbar
-                    open={snackbar.open}
-                    autoHideDuration={4000}
-                    onClose={() => setSnackbar({ ...snackbar, open: false })}
-                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                >
-                    <Alert onClose={() => setSnackbar({ ...snackbar, open: false })} severity={snackbar.severity} sx={{ width: '100%' }}>
-                        {snackbar.message}
-                    </Alert>
-                </Snackbar>
-                <Stack spacing={2} sx={{ mt: 1 }}>
-
+                <Stack spacing={2} sx={{mt: 1}}>
                     <TextField
                         label="Name"
                         fullWidth
                         value={formData.name}
-                        onChange={(e) => setFormData({...formData, name: e.target.value})}
+                        name="name"
+                        onChange={handleChange}
                         required
                     />
-
                     <TextField
-                        label="Email"
-                        type="email"
+                        label="Email" type="email"
                         fullWidth
                         value={formData.email}
-                        onChange={(e) => setFormData({...formData, email: e.target.value})}
+                        name="email"
+                        onChange={handleChange}
                         required
                     />
-
                     <TextField
-                        label="Password"
-                        type="password"
+                        label="Password" type="password"
                         fullWidth
                         value={formData.password}
-                        onChange={(e) => setFormData({...formData, password: e.target.value})}
+                        name="password"
+                        onChange={handleChange}
                         required
                     />
-
-                    <FormControl fullWidth>
+                    <FormControl fullWidth required>
                         <InputLabel>Role</InputLabel>
-                        <Select
-                            value={formData.role}
-                            label="Role"
-                            onChange={(e) => setFormData({
-                                ...formData,
-                                role: e.target.value as IUserCreateData['role']
-                            })}
-                        >
+                        <Select value={formData.role} label="Role" onChange={handleRoleChange}>
                             <MenuItem value="ADMIN">Admin</MenuItem>
-                            <MenuItem value="TEAM_LEAD">Team Lead</MenuItem>
                             <MenuItem value="MEMBER">Member</MenuItem>
                         </Select>
                     </FormControl>
-
-                    <FormControl fullWidth>
-                        <InputLabel>Team Name</InputLabel>
-                        <Select
-                            label="Team Name"
-                            value={formData.team_id || ''}
-                            onChange={e =>
-                                setFormData({ ...formData, team_id: e.target.value })
-                            }
-                        >
-                            {teams.map((team : ITeam) => (
-                                <MenuItem key={team.id} value={team.id}>
-                                    {team.name} ({team.member_count})
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
+                    {formData.role === 'MEMBER' && (
+                        <FormControl fullWidth>
+                            <InputLabel>Team Name</InputLabel>
+                            <Select label="Team Name" value={formData.team_id || ''} onChange={handleSelectChange}>
+                                {teams.length === 0 && <MenuItem value="">No Team Available</MenuItem>}
+                                {teams.map((team: ITeam) => (
+                                    <MenuItem key={team.id} value={team.id}>
+                                        {team.name} ({team.member_count})
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
                 </Stack>
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose} disabled={loading}>Cancel</Button>
-                <Button
-                    onClick={handleSubmit}
-                    variant="contained"
-                    disabled={loading}
-                >
+                <Button onClick={handleClose} disabled={loading}>
+                    Cancel
+                </Button>
+                <Button onClick={createNewUser} variant="contained" disabled={loading}>
                     {loading ? 'Creating...' : 'Create User'}
                 </Button>
             </DialogActions>
