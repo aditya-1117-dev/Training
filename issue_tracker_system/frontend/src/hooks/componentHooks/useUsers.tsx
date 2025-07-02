4475f7fa6bb795db863684f2cc6d2e148f47c676
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-07-02 17:46:34 +0530

    Fix : Column width & role label in Users table

--- issue_tracker_system/frontend/src/hooks/componentHooks/useUsers.tsx
+++ issue_tracker_system/frontend/src/hooks/componentHooks/useUsers.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {Chip, IconButton, Stack} from '@mui/material';
+import {type ChangeEvent, useEffect, useState} from 'react';
+import {Chip, IconButton, type SelectChangeEvent, Stack} from '@mui/material';
 import {Delete, Edit} from '@mui/icons-material';
 import type {IUser, IUserUpdateData} from '../../types/user.ts';
 import type {ITeam} from '../../types/team.ts';
@@ -44,6 +44,16 @@ export const useUsers = () => {
         },
     });
 
+    const {execute: updateUser, isLoading: userUpdating} = useAPI<IUser, IUserUpdateData>('/api/users/:id', {
+        method: 'PUT',
+        onSuccess: () => {
+            addSnackbar({severity: 'success', message: 'User deleted successfully'});
+        },
+        onError: (err: unknown) => {
+            addSnackbar({severity: 'error', message: err instanceof Error ? err.message : 'Delete operation failed'});
+        },
+    });
+
     useEffect(() => {
         fetchUsers();
     }, [page, limit, debouncedSearchTerm, teamFilter]);
@@ -60,16 +70,6 @@ export const useUsers = () => {
     const renderRowNumber = (index: number = 0, page: number, limit: number) => {
         return (page - 1) * limit + index + 1;
     };
-
-    const {execute: updateUser, isLoading: userUpdating} = useAPI<IUser, IUserUpdateData>('/api/users/:id', {
-        method: 'PUT',
-        onSuccess: () => {
-            addSnackbar({severity: 'success', message: 'User deleted successfully'});
-        },
-        onError: (err: unknown) => {
-            addSnackbar({severity: 'error', message: err instanceof Error ? err.message : 'Delete operation failed'});
-        },
-    });
 
     const handleDeleteUser = async (user: IUser) => {
         await updateUser({pathParams: {id: user.id}, body: {...user, is_active: false}})
@@ -98,10 +98,10 @@ export const useUsers = () => {
             key: 'row_number', label: 'ID', width: '5%',
             render: (_, index) => renderRowNumber(index, page, limit),
         },
-        {key: 'name', label: 'Name', width: '10%'},
-        {key: 'email', label: 'Email', width: '15%'},
+        {key: 'name', label: 'Name', width: '15%'},
+        {key: 'email', label: 'Email', width: '20%'},
         {
-            key: 'is_active', label: 'Active Status', width: '15%',
+            key: 'is_active', label: 'Status', width: '10%',
             render: (user: IUser) => {
                 return user.is_active ? (
                     <Chip label="Active" size="small"
@@ -113,19 +113,24 @@ export const useUsers = () => {
             }
         },
         {
-            key: 'role', label: 'Role', width: '15%', align: 'center',
+            key: 'role', label: 'User Role', width: '20%', align: 'center',
             render: (user: IUser) => {
                 const roleColors = {
                     ADMIN: {'backgroundColor': 'rgba(244, 67, 54, 0.1)', 'color': 'rgba(244, 67, 54, 1)',},
                     TEAM_LEAD: {'backgroundColor': 'rgba(33, 150, 243, 0.1)', 'color': 'rgba(33, 150, 243, 1)',},
                     MEMBER: {'backgroundColor': 'rgba(158, 158, 158, 0.1)', 'color': 'rgba(97, 97, 97, 1)',}
                 }
-                return <Chip label={user.role} size="small"
+                const role = {
+                    ADMIN : 'Admin',
+                    TEAM_LEAD: 'Team Lead',
+                    MEMBER: 'Member',
+                }
+                return <Chip label={role[user.role]} size="small"
                              sx={{py: 2, ...roleColors[user.role] }}/>;
             },
         },
         {
-            key: 'team_name', label: 'Team', width: '15%',
+            key: 'team_name', label: 'Team', width: '15%', align: 'center',
             render: (user: IUser) => user.team_name || '-',
         },
         {
@@ -148,14 +153,14 @@ export const useUsers = () => {
         search: {
             value: searchTerm,
             label: 'Search Users',
-            onChange: (e) => setSearchTerm(e.target.value),
+            onChange: (e : ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value),
         },
         filters: [
             {
                 key: 'team',
                 label: 'Team',
                 value: teamFilter,
-                onChange: (e) => {
+                onChange: (e : SelectChangeEvent) => {
                     handlePageChange(1);
                     setTeamFilter(e.target.value);
                 },
