ccba179a7b935a5a33f8f81d9dbb744187049b28
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-07-02 14:48:16 +0530

    Refactor : Use PageContainer in KanbanBorad & separate filters logic part

--- issue_tracker_system/frontend/src/hooks/componentHooks/useKanbanBoard.ts
+++ issue_tracker_system/frontend/src/hooks/componentHooks/useKanbanBoard.ts
@@ -1,42 +1,42 @@
-import { useState, type ChangeEvent } from 'react';
-import type { SelectChangeEvent } from '@mui/material';
-import type { ITask, TTaskStatus } from '../../types/task.ts';
-import type { ITeam } from '../../types/team.ts';
-import type { IUser } from '../../types/user.ts';
+import {useState, type ChangeEvent} from 'react';
+import type {SelectChangeEvent} from '@mui/material';
+import type {ITask, TTaskStatus} from '../../types/task.ts';
+import type {ITeam} from '../../types/team.ts';
+import type {IUser} from '../../types/user.ts';
 import {useAuth} from "../customHooks/useAuth.ts";
 import {useDebounce} from "../customHooks/useDebounce.ts";
 import {useAPI} from "../customHooks/useAPI.ts";
 
 const columns: { status: TTaskStatus; title: string }[] = [
-    { status: 'TODO', title: 'To Do' },
-    { status: 'IN_PROGRESS', title: 'In Progress' },
-    { status: 'IN_REVIEW', title: 'In Review' },
-    { status: 'DONE', title: 'Done' },
+    {status: 'TODO', title: 'To Do'},
+    {status: 'IN_PROGRESS', title: 'In Progress'},
+    {status: 'IN_REVIEW', title: 'In Review'},
+    {status: 'DONE', title: 'Done'},
 ];
 
 export const useKanbanBoard = () => {
-    const { user } = useAuth();
+    const {user} = useAuth();
     const [assigneeFilter, setAssigneeFilter] = useState<string>('all');
     const [teamFilter, setTeamFilter] = useState<string>('all');
     const [selectedTask, setSelectedTask] = useState<ITask | null>(null);
     const [openCreateModal, setOpenCreateModal] = useState(false);
-    const { value: searchTask, setValue: setSearchTask, debouncedValue } = useDebounce<string>('', 1000);
+    const {value: searchTask, setValue: setSearchTask, debouncedValue} = useDebounce<string>('', 1000);
 
-    const { data: teams } = useAPI<ITeam[]>('/api/teams', { method: 'GET' });
-    const { data: users } = useAPI<IUser[]>('/api/users', { method: 'GET' });
-    const { data: tasks, execute: fetchTasks } = useAPI<ITask[]>('/api/tasks', {
+    const {data: teams} = useAPI<ITeam[]>('/api/teams', {method: 'GET'});
+    const {data: users} = useAPI<IUser[]>('/api/users', {method: 'GET'});
+    const {data: tasks, execute: fetchTasks} = useAPI<ITask[]>('/api/tasks', {
         method: 'GET',
         params: {
             search: debouncedValue,
-            assignee_id: assigneeFilter ==='all' ? '' : assigneeFilter,
-            team_id: (user?.role === 'MEMBER' ? user?.team_id : (teamFilter==='all' ? '' : teamFilter)) || '',
+            assignee_id: assigneeFilter === 'all' ? '' : assigneeFilter,
+            team_id: (user?.role === 'MEMBER' ? user?.team_id : (teamFilter === 'all' ? '' : teamFilter)) || '',
         },
     });
 
     const allTeamLeadsAndMembers = users?.filter((user: IUser) => user.role === 'TEAM_LEAD' || user.role === 'MEMBER') || [];
     const membersOfSelectedTaskTeam = allTeamLeadsAndMembers.filter((user: IUser) => selectedTask?.team_id === user.team_id);
-    const assigneeFilterOptions = allTeamLeadsAndMembers.map((user) => ({ value: user.id,  label: user.name } ) );
-    const teamFilterOptions = teams?.map((team : ITeam) => ({ value: team.id, label: team.name })) || []
+    const assigneeFilterOptions = allTeamLeadsAndMembers.map((user) => ({value: user.id, label: user.name}));
+    const teamFilterOptions = teams?.map((team: ITeam) => ({value: team.id, label: team.name})) || []
 
     const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => setSearchTask(e.target.value);
     const handleAssigneeChange = (e: SelectChangeEvent) => setAssigneeFilter(e.target.value);
@@ -57,22 +57,43 @@ export const useKanbanBoard = () => {
         if (fetchTasks) fetchTasks();
     };
 
+    const filterConfig = {
+        search: {
+            value: searchTask,
+            label: 'Search Tasks',
+            onChange: handleSearchChange
+        },
+        filters: [
+            {
+                key: 'assignee',
+                label: 'Assignee',
+                value: assigneeFilter,
+                onChange: handleAssigneeChange,
+                options: assigneeFilterOptions,
+            },
+        ]
+    };
+
+    if (user?.role !== 'MEMBER') {
+        filterConfig.filters.push({
+            key: 'team',
+            label: 'Team',
+            value: teamFilter,
+            onChange: handleTeamChange,
+            options: teamFilterOptions,
+        });
+    }
+
     return {
         user,
         columns,
         tasks,
         teams,
         users,
-        assigneeFilter,
-        teamFilter,
         selectedTask,
         openCreateModal,
-        searchTask,
         allTeamLeadsAndMembers,
         membersOfSelectedTaskTeam,
-        handleSearchChange,
-        handleAssigneeChange,
-        handleTeamChange,
         handleTaskClick,
         handleCreateModalOpen,
         handleCreateModalClose,
@@ -81,6 +102,7 @@ export const useKanbanBoard = () => {
         handleCreateTask,
         handleOnTaskDrop,
         teamFilterOptions,
-        assigneeFilterOptions
+        assigneeFilterOptions,
+        filterConfig
     };
 };
