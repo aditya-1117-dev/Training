1251d6dd2ef2974f023bc3bf9442588fb0555d61
Aditya-Gaikwad-Noovosoft <aditya.gaikwad@noovosoft.com>
2025-07-07 17:04:20 +0530

    Fix loading for dialog

--- issue_tracker_system/frontend/src/components/EditUserDialog.tsx
+++ issue_tracker_system/frontend/src/components/EditUserDialog.tsx
@@ -4,7 +4,7 @@ import {
     Stack,
     FormControl,
     InputLabel,
-    Select, CircularProgress,
+    Select,
 } from '@mui/material';
 import type {IUser} from '../types/user.ts';
 import type {ITeam} from '../types/team.ts';
@@ -36,70 +36,67 @@ export function EditUserDialog({open, onClose, onSubmit, teams, user}: IEditUser
             onSubmit={handleSubmit}
             title="Edit User"
             submitButtonText={loading ? 'Updating...' : 'Update User'}
-            loading={loading}
+            loading={loading || !user}
             maxWidth="sm"
         >
-            {!user
-                ? <CircularProgress/>
-                : <Stack spacing={2} sx={{mt: 1}}>
-                    <TextField
-                        label="Name"
-                        fullWidth
-                        value={formData.name}
-                        onChange={(e) => setFormData({...formData, name: e.target.value})}
-                        required
-                    />
-                    <TextField
-                        label="Email"
-                        type="email"
-                        fullWidth
-                        value={formData.email}
-                        onChange={(e) => setFormData({...formData, email: e.target.value})}
-                        required
-                    />
-                    <TextField
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        value={formData.password}
-                        onChange={(e) => setFormData({...formData, password: e.target.value})}
-                    />
+            <Stack spacing={2} sx={{mt: 1}}>
+                <TextField
+                    label="Name"
+                    fullWidth
+                    value={formData.name}
+                    onChange={(e) => setFormData({...formData, name: e.target.value})}
+                    required
+                />
+                <TextField
+                    label="Email"
+                    type="email"
+                    fullWidth
+                    value={formData.email}
+                    onChange={(e) => setFormData({...formData, email: e.target.value})}
+                    required
+                />
+                <TextField
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    value={formData.password}
+                    onChange={(e) => setFormData({...formData, password: e.target.value})}
+                />
 
-                    {user?.role !== 'TEAM_LEAD' && (
-                        <FormControl fullWidth>
-                            <InputLabel>Role</InputLabel>
-                            <Select
-                                value={formData.role !== 'TEAM_LEAD' ? formData.role : ''}
-                                label="Role"
-                                onChange={handleRoleChange}
-                            >
-                                <MenuItem value="ADMIN">Admin</MenuItem>
-                                <MenuItem value="MEMBER">Member</MenuItem>
-                            </Select>
-                        </FormControl>
-                    )}
+                {user?.role !== 'TEAM_LEAD' && (
+                    <FormControl fullWidth>
+                        <InputLabel>Role</InputLabel>
+                        <Select
+                            value={formData.role !== 'TEAM_LEAD' ? formData.role : ''}
+                            label="Role"
+                            onChange={handleRoleChange}
+                        >
+                            <MenuItem value="ADMIN">Admin</MenuItem>
+                            <MenuItem value="MEMBER">Member</MenuItem>
+                        </Select>
+                    </FormControl>
+                )}
 
-                    {formData.role === 'MEMBER' && (
-                        <FormControl fullWidth>
-                            <InputLabel>Team Name</InputLabel>
-                            <Select
-                                label="Team Name"
-                                value={formData.team_id || ''}
-                                onChange={(e) => setFormData({...formData, team_id: e.target.value})}
-                            >
-                                {teams.length === 0 && (
-                                    <MenuItem value="">No Team Available</MenuItem>
-                                )}
-                                {teams.map((team: ITeam) => (
-                                    <MenuItem key={team.id} value={team.id}>
-                                        {team.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    )}
-                </Stack>
-            }
+                {formData.role === 'MEMBER' && (
+                    <FormControl fullWidth>
+                        <InputLabel>Team Name</InputLabel>
+                        <Select
+                            label="Team Name"
+                            value={formData.team_id || ''}
+                            onChange={(e) => setFormData({...formData, team_id: e.target.value})}
+                        >
+                            {teams.length === 0 && (
+                                <MenuItem value="">No Team Available</MenuItem>
+                            )}
+                            {teams.map((team: ITeam) => (
+                                <MenuItem key={team.id} value={team.id}>
+                                    {team.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                )}
+            </Stack>
         </DialogForm>
     );
 }
